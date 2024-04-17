79a3898c9a241b2c5ac4d261ef379436b6d21292
Paschal <59150676+paxxchal@users.noreply.github.com>
2024-04-17 10:48:24 +0100

    update file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,33 +5,39 @@ import json
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
-
-    __file_path = "file.json"
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns the dictionary __objects"""
+        """Returns a dictionary of models currently in storage"""
         if cls is None:
-            return self.__objects
-        else:
-            filtered_objs = {}
-            for key, obj in self.__objects.items():
-                if type(obj) == cls:
-                    filtered_objs[key] = obj
-            return filtered_objs
+            return FileStorage.__objects
+        return {key: value for key, value in FileStorage.__objects.items()
+                if cls.__name__ == key.split('.')[0]}
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()["__class__"] + "." + obj.id: obj})
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, "w") as f:
+        with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside"""
+        if obj is None:
+            return
+
+        new_storage_object = {}
+        for key, value in FileStorage.__objects.items():
+            if obj is not value:
+                new_storage_object[key] = value
+        FileStorage.__objects = new_storage_object
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -44,29 +50,21 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-            "BaseModel": BaseModel,
-            "User": User,
-            "Place": Place,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Review": Review,
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
         }
         try:
             temp = {}
-            with open(FileStorage.__file_path, "r") as f:
+            with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val["__class__"]](**val)
+                    """self.all()[key] = classes[val['__class__']](**val)"""
+                    FileStorage.__objects[key] = classes[val['__class__']](
+                        **val)
         except FileNotFoundError:
             pass
 
-    def delete(self, obj=None):
-        """
-        Deletes obj from __objects if it's inside
-        If obj is None, the method should not do anything
-        """
-        if obj is not None:
-            key = f"{obj.__class__.__name__}.{obj.id}"
-            if key in self.__objects:
-                del self.__objects[key]
+    def close(self):
+        """Calls reload() method for deserializing the JSON file to objects"""
+        self.reload()
